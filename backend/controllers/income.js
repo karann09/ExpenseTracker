5630b7f4336dca8e75d6d099b28f11df5514f81b
karann09 <karankumar011kp@gmail.com>
2024-05-18 17:46:34 +0530

    created routes

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,10 +1,10 @@
-const incomeSchema = require("../models/incomeModel");
+const IncomeSchema = require("../models/incomeModel");
 
 
 exports.addIncome = async (req, res)=>{
     const {title, amount, category, description, date} = req.body
 
-    const income = incomeSchema({
+    const income = IncomeSchema({
         title,
         amount,
         category,
@@ -16,8 +16,8 @@ exports.addIncome = async (req, res)=>{
         if(!title || !amount || !category || !description || !date){
             return res.status(400).json({message: "All fields are mandatory"});
         }
-        if( amount <= 0 || !amount === 'number'){
-            return res.status(400).json({message: "Amount must be a positive number"});
+        if (isNaN(amount) || amount <= 0) {
+            return res.status(400).json({ message: 'Amount must be a positive number!' });
         }
         await income.save();
         res.status(200).json({message: "Income Added"})
@@ -25,4 +25,23 @@ exports.addIncome = async (req, res)=>{
         res.status(500).json({message:"Server Error"});
 }
     console.log(income);
+}
+
+exports.getIncome = async (req, res)=> {
+    try {
+        const incomes = await IncomeSchema.find()
+        res.status(200).json(incomes);
+    } catch (error) {
+        res.status(500).json({message:"Server Error"});
+    }
+}
+exports.deleteIncome = async (req, res)=> {
+    const {id} = req.params;
+    IncomeSchema.findByIdAndDelete(id)
+    .then((income)=> {
+        res.status(200).json({message:"Income deleted successfully"});
+    })
+    .catch((error)=>{
+        res.status(500).json({message:"Server Error"});
+    })
 }
