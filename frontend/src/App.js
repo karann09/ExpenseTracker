20641afcc92b9ac4ceeae894867a3d6287851899
karann09 <karankumar011kp@gmail.com>
2024-05-24 16:17:20 +0530

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import bg from "./images/bg.png"
+
+function App() {
+  return (
+    <AppStyled bg={bg}className="App">
+      
+    </AppStyled>
+  );
+}
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
+`;
+
+
+export default App;
