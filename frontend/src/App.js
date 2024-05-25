84e3c77b85f174f2ce8c9bfd6b48f5c47cb13342
karann09 <karankumar011kp@gmail.com>
2024-05-25 21:46:03 +0530

    layout initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 import bg from "./images/bg.png"
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./components/Orb/Orb";
 
 function App() {
   return (
     <AppStyled bg={bg}className="App">
+      <Orb/>
+    <MainLayout>
+      
+    </MainLayout>
       
     </AppStyled>
   );
